906c9813bd7abaf1b4fd88479bae08a50a332684
sky0014 <sky-wang@qq.com>
2016-04-11 01:21:25 +0800

    增加注释

--- decorator.py
+++ decorator.py
@@ -1,3 +1,7 @@
+# http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/0014318435599930270c0381a3b44db991cd6d858064ac0000
+# 装饰器学习
+# by sky0014
+
 from functools import wraps
 
 
